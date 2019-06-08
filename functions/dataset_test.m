a2095268d1cceac708af7085fea33cb1922d5922
Terry <terryluohello@qq.com>
2019-06-09 03:08:47 +0800

    Update the saved workspace variables format.

--- functions/dataset_test.m
+++ functions/dataset_test.m
@@ -1,4 +1,4 @@
-function [date_result] = dataset_test(file_path_im,file_path_manual,im_postfix,ma_postfix)
+function [data_result] = dataset_test(file_path_im,file_path_manual,im_postfix,ma_postfix)
 %对数据集内的大量数据进行测试,并生成测量结果
 %   输入：
 %           file_path_im：测试图像路径
@@ -14,7 +14,7 @@ img_path_list_im = dir(strcat(file_path_im,im_postfix));
 img_path_list_manual = dir(strcat(file_path_manual,ma_postfix)); 
 
 img_num = length(img_path_list_im); % 获取图像总数量
-date_result = struct([]);            % 初始化测试结果
+data_result = struct([]);            % 初始化测试结果
 
 if img_num > 0 % 有满足条件的图像
         for i = 1:img_num % 逐一读取图像
@@ -27,14 +27,14 @@ if img_num > 0 % 有满足条件的图像
             fprintf('%d %s\n',i,strcat(file_path_im,image_name));
             
             %图像处理过程 
-            date_result(i).image = image_name;
-            date_result(i).manual = manual_name;
+            data_result(i).image = image_name;
+            data_result(i).manual = manual_name;
             
             [Se, Sp, ~, ~, Acc, Dice] = retinal_vessel_seg(image,manual);
-            date_result(i).Accuracy = Acc;
-            date_result(i).Sensitivity = Se;
-            date_result(i).Specificity = Sp;
-            date_result(i).Dice = Dice;
+            data_result(i).Accuracy = Acc;
+            data_result(i).Sensitivity = Se;
+            data_result(i).Specificity = Sp;
+            data_result(i).Dice = Dice;
         end
 end
 
--- run_me.m
+++ run_me.m
@@ -21,8 +21,8 @@ file_path_im = './data/Images/DRIVE/test/images/';         % 测试图像路径
 file_path_manual = './data/Images/DRIVE/test/1st_manual/'; % 参考图像路径
 im_postfix = '*.tif';     % 测试图像后缀
 ma_postfix = '*.gif';     % 参考图像后缀
-[date_drive] = dataset_test(file_path_im, file_path_manual, im_postfix, ma_postfix);
-save date_drive           % 保存测试结果
+[result_drive] = dataset_test(file_path_im, file_path_manual, im_postfix, ma_postfix);
+save('result_drive.mat','result_drive') % 保存测试结果
 
 % =========================================================================
 % 测试二 STARE
@@ -31,8 +31,8 @@ save date_drive           % 保存测试结果
 % file_path_manual = './data/Images/STARE/groundtruth_STARE_ah/'; % 参考图像路径
 % im_postfix = '*.ppm';     % 测试图像后缀
 % ma_postfix = '*.ppm';     % 参考图像后缀
-% [date_stare] = dataset_test(file_path_im, file_path_manual, im_postfix, ma_postfix);
-% save date_stare           % 保存测试结果
+% [result_stare] = dataset_test(file_path_im, file_path_manual, im_postfix, ma_postfix);
+% save('result_stare.mat','result_stare') % 保存测试结果
 
 % =========================================================================
 % 测试三 CHASEDB1
@@ -41,8 +41,8 @@ save date_drive           % 保存测试结果
 % file_path_manual = './data/Images/CHASEDB1/groundtruth1_CHASEDB1/'; % 参考图像路径  
 % im_postfix = '*.jpg';     % 测试图像后缀
 % ma_postfix = '*.png';     % 参考图像后缀
-% [date_CHASEDB1] = dataset_test(file_path_im, file_path_manual, im_postfix, ma_postfix);
-% save date_CHASEDB1        % 保存测试结果
+% [result_CHASEDB1] = dataset_test(file_path_im, file_path_manual, im_postfix, ma_postfix);
+% save('result_CHASEDB1.mat','result_CHASEDB1') % 保存测试结果
 
 %% 结束计时
 toc;
